b3ca794ee985f1811bc9fc7fa0c2a20b0a3a9b44
Wijdannn <113024760+Wijdannn@users.noreply.github.com>
2023-11-22 12:42:49 +0700

    Add files via upload

---
+++ looping/script.js
@@ -0,0 +1,17 @@
+// Looping For
+for(let i = 1; i <= 10; i ++){
+  console.log(`this is for ${i}`)
+}
+// Looping While
+let p = 1;
+while(p <= 10){
+  console.log(`this is while ${p}`)
+  p++
+}
+
+// Looping do while
+let x = 1
+do {
+  console.log(`this is do while ${x}`)
+  x++
+} while (x <= 10);
